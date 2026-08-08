91e2b6a1eda43468862452e58591b5b9db5e64e3
Axymorrsen <Axymosen@outlook.com>
2026-08-08 14:58:53 +0800

    fix(device): parse restore receipt booleans correctly

--- module/device_validation.sh
+++ module/device_validation.sh
@@ -50,7 +50,7 @@ json_number() {
 json_bool() {
     _pn_key=$1
     _pn_file=$2
-    grep -o "\"${_pn_key}\"[[:space:]]*:[[:space:]]*(true|false)" "$_pn_file" 2>/dev/null \
+    grep -Eo "\"${_pn_key}\"[[:space:]]*:[[:space:]]*(true|false)" "$_pn_file" 2>/dev/null \
         | head -n 1 \
         | sed -E "s/.*\"${_pn_key}\"[[:space:]]*:[[:space:]]*(true|false).*/\\1/"
 }
